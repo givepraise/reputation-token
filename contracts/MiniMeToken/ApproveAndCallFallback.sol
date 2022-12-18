11c264b1881dafeebaecc9f31a76246a01f9f2cc
Kurt <Kurt@Merbeth.io>
2022-12-18 14:39:35 +0100

    removed minime, new specs

--- contracts/MiniMeToken/ApproveAndCallFallback.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-abstract contract ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 _amount, address _token, bytes memory _data) public virtual;
-}
